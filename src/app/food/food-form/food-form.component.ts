8344a53360ec6e7ca745d5e688e67ae26162937b
alexmrtc <alexmrtc@gmail.com>
2024-04-27 20:37:09 +0200

    Create form structure

---
+++ src/app/food/food-form/food-form.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-form',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-form.component.html',
+  styleUrl: './food-form.component.scss'
+})
+export class FoodFormComponent {
+
+}
