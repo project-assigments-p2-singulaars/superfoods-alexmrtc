e9c4c5eecc2833751b31cacc0c4fbb0ddcbb1c53
LPCBCN044 <alexmrtc@gmail.com>
2024-05-06 09:17:09 +0200

    Fix multiple quantity error

--- src/app/food/components/food-details/food-details.component.ts
+++ src/app/food/components/food-details/food-details.component.ts
@@ -1,12 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Foods } from '../../../shared/interfaces/foods';
 
 @Component({
   selector: 'app-food-details',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './food-details.component.html',
   styleUrl: './food-details.component.scss'
 })
 export class FoodDetailsComponent {
+  @Input() foodElement!:Foods;
+  @Output() sendDish = new EventEmitter();
 
+  foodQuantity=1;
+
+  quantity:number = 1;
+
+  addDishToMeal(dish: Foods){
+    const selectedDish = {...dish, quantity: this.foodQuantity}
+    this.sendDish.emit(selectedDish)
+  }
 }
