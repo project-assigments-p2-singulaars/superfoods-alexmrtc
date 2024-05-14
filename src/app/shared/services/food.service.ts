77854ed9325433f5cc464ef4b076e982d464ea9e
LPCBCN044 <alexmrtc@gmail.com>
2024-05-14 09:40:39 +0200

    Remove unnecessary function at food.service.ts

--- src/app/shared/services/food.service.ts
+++ src/app/shared/services/food.service.ts
@@ -19,20 +19,4 @@ export class FoodService {
   getLastId(){
     return foods.at(-1)!.id
   }
-
-  calculateTotalCalories(food:Food[]) {
-    let totalCalories = 0;
-
-    const foodCalories:number[] = [];
-
-    // food.map((dish) => foodCalories.push(dish.calories * dish.quantity)
-    // )
-
-    // totalCalories = foodCalories.reduce((accumulator, currentValue) => 
-    //   accumulator + currentValue, totalCalories
-    // );
-
-    totalCalories = food.reduce((accumulator, currentValue) => accumulator + (currentValue.calories * currentValue.quantity), totalCalories);
-    return totalCalories;
-  }
 }
